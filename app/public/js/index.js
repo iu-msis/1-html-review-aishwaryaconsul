96d035d1947c2d4eeb3976364500e308537597f3
aishwaryaconsul <89750776+aishwaryaconsul@users.noreply.github.com>
2021-09-24 18:21:53 -0400

    Committed changes from local branch to iu-msis branch

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,58 +1,36 @@
 const app = {
     data() {
       return {
-          "user": {},
-          "image": "",
-          "firstname":"",
-          "lastname":"",
-          "dob":"",
-          "age":"",
-          "origin":"",
-          "email":"",
-          "cell":"",
-          "snumber":"",
-          "sname":"",
-          "postcode":"",
+        result: undefined,
         app: 0
       }
     },
   
-     computed:{
-       prettyBirthday(){
-         return dayjs(this.dob.date).format('D MMM YYYY')
-       }
-     },
-
-     methods:{
-       fetchUserData(){
-
-       }
-     },
-
-  created() {
-
-  fetch('https://randomuser.me/api/')
-  .then(response => response.json())
-  .then(data => 
-    {
-        console.log(data);
-        var x= data.results[0];
-        this.image=x.picture.large;
-        this.firstname=x.name.first;
-        this.lastname=x.name.last;
-        this.dob=x.dob.date;
-        this.age=x.dob.age;
-        this.origin=x.location.country;
-        this.email=x.email;
-        this.cell=x.cell;
-        this.snumber=x.location.street.number;
-        this.sname=x.location.street.name;
-        this.postcode=x.location.postcode;
+     computed: {
+        prettyBirthday() {
+            return dayjs(this.result.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            //Method 1:
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.result = json.results[0];
+                console.log("C");
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchUserData();
     }
-    
-    )
-  
 
-}
-}
+  }
+  
   Vue.createApp(app).mount('#app');
